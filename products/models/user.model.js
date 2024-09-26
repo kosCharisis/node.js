3966f0275ed6efd5bd22ba4cdbb0467fdd7ab7dd
Konstantinos Charisis <harisis_kostas@hotmail.com>
2024-09-26 20:06:20 +0300

    crud

--- products/models/user.model.js
+++ products/models/user.model.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const uniqueValidator= require('mongoose-unique-validator')
 
 const Schema = mongoose.Schema;
 
@@ -63,5 +64,7 @@ let userSchema = new Schema({
     collection: 'users',
     timestamps: true 
 });
+
+userSchema.plugin(uniqueValidator);
 
 module.exports = mongoose.model('User', userSchema)
